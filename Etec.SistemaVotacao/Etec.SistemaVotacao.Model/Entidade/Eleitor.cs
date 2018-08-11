00f577f35f27bbbe0d5e73125c531730a5e645b6
SarahSouzaR <sarinha_desouza@outlook.com>
2018-08-11 13:00:46 -0300

    Projeto com a Separação do MVC

---
+++ Etec.SistemaVotacao/Etec.SistemaVotacao.Model/Entidade/Eleitor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Etec.SistemaVotacao.Model.Entidade
+{
+    public class Eleitor
+    {
+        public int cdEleitor { get; set; }
+
+        public string nmEleitor { get; set; }
+
+    }
+}
