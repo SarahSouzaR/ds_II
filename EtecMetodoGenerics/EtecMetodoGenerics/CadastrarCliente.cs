919f3c326ec51dbf126f84a1dc7a93131b7d4074
SarahSouzaR <sarinha_desouza@outlook.com>
2018-08-18 22:05:06 -0300

    Cadastrando através de uma lista

---
+++ EtecMetodoGenerics/EtecMetodoGenerics/CadastrarCliente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Classes;
+
+namespace EtecMetodoGenerics
+{
+    public partial class CadastrarCliente : Form
+    {
+        List<Cliente> listaClientesCadastrados = new List<Cliente>();
+
+        public CadastrarCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            Cliente clienteCadastro = new Cliente();
+            clienteCadastro.codigoCliente = Convert.ToInt32(txtCodigoCliente.Text);
+            clienteCadastro.nmCliente = txtNomeCliente.Text;
+
+            listaClientesCadastrados.Add(clienteCadastro);
+
+            dgvClienteCadastrado.DataSource = null;
+            dgvClienteCadastrado.DataSource = listaClientesCadastrados;
+
+        }
+
+        private void CadastrarCliente_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ EtecMetodoGenerics/EtecMetodoGenerics/Pedidos.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace EtecMetodoGenerics
+{
+    partial class Pedidos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gdvPedidos = new System.Windows.Forms.DataGridView();
+            this.idPedidoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nmProdutoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dtPedidoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.precoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.marcaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clientePedidoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.gdvPedidos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientePedidoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gdvPedidos
+            // 
+            this.gdvPedidos.AutoGenerateColumns = false;
+            this.gdvPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gdvPedidos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idPedidoDataGridViewTextBoxColumn,
+            this.nmProdutoDataGridViewTextBoxColumn,
+            this.dtPedidoDataGridViewTextBoxColumn,
+            this.precoDataGridViewTextBoxColumn,
+            this.marcaDataGridViewTextBoxColumn});
+            this.gdvPedidos.DataSource = this.clientePedidoBindingSource;
+            this.gdvPedidos.Location = new System.Drawing.Point(22, 42);
+            this.gdvPedidos.Name = "gdvPedidos";
+            this.gdvPedidos.Size = new System.Drawing.Size(545, 195);
+            this.gdvPedidos.TabIndex = 0;
+            // 
+            // idPedidoDataGridViewTextBoxColumn
+            // 
+            this.idPedidoDataGridViewTextBoxColumn.DataPropertyName = "idPedido";
+            this.idPedidoDataGridViewTextBoxColumn.HeaderText = "idPedido";
+            this.idPedidoDataGridViewTextBoxColumn.Name = "idPedidoDataGridViewTextBoxColumn";
+            // 
+            // nmProdutoDataGridViewTextBoxColumn
+            // 
+            this.nmProdutoDataGridViewTextBoxColumn.DataPropertyName = "nmProduto";
+            this.nmProdutoDataGridViewTextBoxColumn.HeaderText = "nmProduto";
+            this.nmProdutoDataGridViewTextBoxColumn.Name = "nmProdutoDataGridViewTextBoxColumn";
+            // 
+            // dtPedidoDataGridViewTextBoxColumn
+            // 
+            this.dtPedidoDataGridViewTextBoxColumn.DataPropertyName = "dtPedido";
+            this.dtPedidoDataGridViewTextBoxColumn.HeaderText = "dtPedido";
+            this.dtPedidoDataGridViewTextBoxColumn.Name = "dtPedidoDataGridViewTextBoxColumn";
+            // 
+            // precoDataGridViewTextBoxColumn
+            // 
+            this.precoDataGridViewTextBoxColumn.DataPropertyName = "preco";
+            this.precoDataGridViewTextBoxColumn.HeaderText = "preco";
+            this.precoDataGridViewTextBoxColumn.Name = "precoDataGridViewTextBoxColumn";
+            // 
+            // marcaDataGridViewTextBoxColumn
+            // 
+            this.marcaDataGridViewTextBoxColumn.DataPropertyName = "marca";
+            this.marcaDataGridViewTextBoxColumn.HeaderText = "marca";
+            this.marcaDataGridViewTextBoxColumn.Name = "marcaDataGridViewTextBoxColumn";
+            // 
+            // clientePedidoBindingSource
+            // 
+            this.clientePedidoBindingSource.DataSource = typeof(Classes.ClientePedido);
+            // 
+            // Pedidos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 296);
+            this.Controls.Add(this.gdvPedidos);
+            this.Name = "Pedidos";
+            this.Text = "Pedidos";
+            this.Load += new System.EventHandler(this.Pedidos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gdvPedidos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientePedidoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gdvPedidos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idPedidoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nmProdutoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dtPedidoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn precoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn marcaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource clientePedidoBindingSource;
+    }
+}
